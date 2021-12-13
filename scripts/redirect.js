470ce9f77137c521bb09f54a1b08977760e693c8
Emmanuel Chukwurah <emekaemmanuel045@gmail.com>
2021-12-13 22:39:22 +0100

    feature: ECX registration redirection

---
+++ scripts/redirect.js
@@ -0,0 +1,6 @@
+const destinations = {
+  "/register": "https://docs.google.com/forms/d/e/1FAIpQLSfzXgtHzSsLtYlgnOLDQG4kZvqEnZRIHKf_uHD7VchQyWrmiQ/viewform?usp=sf_link",
+};
+
+const { pathname } = new URL(location.href);
+console.log(pathname);
