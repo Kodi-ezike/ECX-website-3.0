ce42b5db1ba1f2656fb550b4acc5687af8a44b64
Emmanuel Chukwurah <emekaemmanuel045@gmail.com>
2022-01-03 23:54:14 +0100

    add redirects for setup-guide

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,10 +1,11 @@
 const routes = {
   "/register": "https://docs.google.com/forms/d/e/1FAIpQLSfzXgtHzSsLtYlgnOLDQG4kZvqEnZRIHKf_uHD7VchQyWrmiQ/viewform?usp=sf_link",
   "/resources": "https://github.com/ECX-Unilag/Learning-path",
-  "/slack": "https://join.slack.com/t/ecxunilag/shared_invite/zt-1116iknia-s4iVcOpd2V9g3VKIoIV6lw"
+  "/slack": "https://join.slack.com/t/ecxunilag/shared_invite/zt-1116iknia-s4iVcOpd2V9g3VKIoIV6lw",
+  "setup-guide": "https://docs.google.com/document/d/1ICTviuLTaWu3RAIikdzkonuhgsJ9p40NtgwiqUi_Zxg/edit?usp=drivesdk"
 };
 
-const path = "/" + location.pathname.split("/").pop();
+const path = "/" + location.pathname.split("/").pop().toLowerCase();
 
 if (routes[path]) {
   location.replace(routes[path]);
