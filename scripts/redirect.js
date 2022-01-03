3884e2ea28690d8490f7d36ab286281b3a427303
Emmanuel Chukwurah <emekaemmanuel045@gmail.com>
2022-01-03 23:28:33 +0100

    redirect for slack invite

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,6 +1,7 @@
 const routes = {
   "/register": "https://docs.google.com/forms/d/e/1FAIpQLSfzXgtHzSsLtYlgnOLDQG4kZvqEnZRIHKf_uHD7VchQyWrmiQ/viewform?usp=sf_link",
-  "/resources": "https://github.com/ECX-Unilag/Learning-path"
+  "/resources": "https://github.com/ECX-Unilag/Learning-path",
+  "/slack": "https://join.slack.com/t/ecxunilag/shared_invite/zt-1116iknia-s4iVcOpd2V9g3VKIoIV6lw"
 };
 
 const path = "/" + location.pathname.split("/").pop();
