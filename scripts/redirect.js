f3c895fac6e4bcec1730cbcfef8d4be91c3abded
Emmanuel Chukwurah <emekaemmanuel045@gmail.com>
2022-01-03 20:26:15 +0100

    add resource redirect link

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,5 +1,6 @@
 const routes = {
   "/register": "https://docs.google.com/forms/d/e/1FAIpQLSfzXgtHzSsLtYlgnOLDQG4kZvqEnZRIHKf_uHD7VchQyWrmiQ/viewform?usp=sf_link",
+  "/resources": "https://github.com/ECX-Unilag/Learning-path"
 };
 
 const path = "/" + location.pathname.split("/").pop();
