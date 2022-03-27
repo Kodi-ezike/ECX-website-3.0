ced4b9b6c0b1d395e072c13018dacb01bad737b9
Emmanuel Chukwurah <emekaemmanuel045@gmail.com>
2022-03-27 17:17:08 +0100

    feat: force push

--- js/404.js
+++ js/404.js
@@ -3,11 +3,10 @@ const routes = {
     "/resources": "https://github.com/ECX-Unilag/Learning-path",
     "/slack": "https://join.slack.com/t/ecxunilag/shared_invite/zt-1116iknia-s4iVcOpd2V9g3VKIoIV6lw",
     "/setup-guide": "https://docs.google.com/document/d/1-MWc8_CX85QhyPplzGXrygCZLnl1qc8Ppr1RxHv25nM/edit?usp=drivesdk",
-    "/meeting-links": "https://docs.google.com/document/d/1_tIs1gcewE-OYRJ_Iw9X1p0kCyHkh1YnUU6ira5Kn0E/edit?usp=sharing"
+    "/meeting-links": "https://docs.google.com/document/d/1_tIs1gcewE-OYRJ_Iw9X1p0kCyHkh1YnUU6ira5Kn0E/edit?usp=sharing",
 };
 
-let path = "/" + location.pathname.split("/").pop();
-path = path.toLowerCase();
+let path = ("/" + location.pathname.split("/").pop()).toLowerCase();
 if (routes[path]) {
     location.replace(routes[path]);
 } else {
